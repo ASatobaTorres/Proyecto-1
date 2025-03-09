3a2ce6adf3fc33e06c4a4ccf2a06869b89914b47
ASatobaTorres <andessatoba0@gmail.com>
2025-03-08 19:42:30 -0500

    Configuracion de firebase hosting con GitHub Actions

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,22 @@
 /**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ * Importa los disparadores de funciones desde sus respectivos submódulos:
+ * 
+ * const { onCall } = require("firebase-functions/v2/https");
+ * const { onDocumentWritten } = require("firebase-functions/v2/firestore");
+ * 
+ * Consulta la lista completa de disparadores compatibles en:
+ * https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const { onRequest } = require("firebase-functions/v2/https"); // Importa la función para manejar solicitudes HTTP
+const logger = require("firebase-functions/logger"); // Importa el módulo para registrar mensajes en Firebase
 
-// Create and deploy your first functions
+// Crea y despliega tu primera función
 // https://firebase.google.com/docs/functions/get-started
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+/*
+exports.helloWorld = onRequest((request, response) => {
+    logger.info("¡Hola logs!", { structuredData: true });
+    response.send("¡Hola desde Firebase!");
+});
+*/
